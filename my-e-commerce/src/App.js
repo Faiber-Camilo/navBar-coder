a9ebb63904a50fc97098a6fbc86a9c13c81aa6b1
Faiber Camilo <camilofcb92@gmail.com>
2021-09-19 20:35:53 -0500

    primer archivo my e-commerce

---
+++ my-e-commerce/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Bienvenido a mi tienda e-commerce, pronto te diré el nombre¡¡</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat aperiam dignissimos accusamus eligendi quibusdam eveniet minus amet, a atque obcaecati itaque odio inventore soluta numquam, vitae facilis expedita tempore odit.</p>
+    </div>
+  );
+}
+
+export default App;
