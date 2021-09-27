e82c388ae306550d08e85faffcdacee3755b56e9
Faiber Camilo <camilofcb92@gmail.com>
2021-09-27 18:55:10 -0500

    navBar

--- my-e-commerce/src/App.js
+++ my-e-commerce/src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Bienvenido a mi tienda e-commerce, pronto te diré el nombre¡¡</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat aperiam dignissimos accusamus eligendi quibusdam eveniet minus amet, a atque obcaecati itaque odio inventore soluta numquam, vitae facilis expedita tempore odit.</p>
+import './App.css';
+import './components/NavBar.css';
+import NavBar from './components/NavBar.js';
+import React ,{Component} from "react";
+
+
+class App extends Component {
+  render(){
+    return (
+    <div className='navBar'>
+      <NavBar />
     </div>
+      
   );
+  }
+  
 }
 
 export default App;
---
+++ my-e-commerce/src/components/NavBar.js
@@ -0,0 +1,16 @@
+import './NavBar.css'
+const NavBar = () => {
+    return(
+        <nav className='navBar'>
+            <ul>
+                <li>NUEVO</li>
+                <li>HOMBRE</li>
+                <li>MUJER</li>
+                <li>NIÑOS</li>
+                <li>SALE</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
